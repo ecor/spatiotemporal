a5d2053373c0970e63d54f8882ecdf2309a328f7
Emanuele Cordano <emanuele.cordano@gmail.com>
2025-07-21 12:14:22 +0200

    first commit

--- inst/rscripts/installation_tensorflow.R
+++
@@ -1,8 +0,0 @@
-# Installare il pacchetto tensorflow
-#install.packages("tensorflow")
-library(tensorflow)
-
-# Installare TensorFlow
-install_tensorflow()
-# Installare il pacchetto kerastools.callback
-reticulate::py_install("kerastools")
